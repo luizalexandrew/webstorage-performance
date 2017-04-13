9efa44c5df74b032fe8af33784e3e7142378c850
Luiz Alexandre <luiz.alexandre@live.com>
2017-04-12 22:13:56 -0300

    Modelo e calculo da diferença entre tempos

--- websql/app.js
+++ websql/app.js
@@ -1,1 +1,43 @@
- 
+(function(){	
+
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+            window.dadosTeste = this.responseText;
+
+            //Operações de testes no banco;
+            insert();
+            // retrieve();
+            // update();
+            // remove();
+           
+        }	
+	};
+	xhttp.open("GET", "./dados.json");
+	xhttp.send();
+		
+})(); 
+
+
+function insert(){
+
+	var tempoInicial = Date.now();
+
+	//Operação no banco de dados
+
+	var tempoFinal = Date.now();
+	console.log((tempoFinal - tempoInicial)/1000);
+}
+
+function retrieve(){
+
+}
+
+function update(){
+
+}
+
+function remove(){
+
+}
