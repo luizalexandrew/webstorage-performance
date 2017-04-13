d94c6da7191bf663b5f012d8fdab3fa4a00c192f
Luiz Alexandre <luiz.alexandre@live.com>
2017-04-13 12:42:35 -0300

    Teste Pouchdb

---
+++ pouchdb/app.js
@@ -0,0 +1,101 @@
+(function(){
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+            window.dadosTeste = JSON.parse(this.responseText);
+
+            //Operações de testes no banco;
+
+            // testeInsert();
+            // testeRetrieve();
+            testeRemove();
+
+        }
+    };
+    xhttp.open("GET", "./dados.json");
+    xhttp.send();
+
+})();
+
+
+function insert(){
+
+    let tempoInicial = performance.now();;
+
+    let db = new PouchDB('db');
+
+    db.put({
+        _id: 'dados',
+        nome: dadosTeste         
+    }).then(function (response) {
+        let tempoFinal = performance.now();;
+        console.log((tempoFinal - tempoInicial)/1000);
+    }).catch(function (err) {
+        console.log("erro");
+    }); 
+
+}
+
+function retrieve(){
+    let tempoInicial = performance.now();;
+    let db = new PouchDB('db');
+
+    db.get('dados').then(function(doc) {
+        let tempoFinal = performance.now();;
+        console.log((tempoFinal - tempoInicial)/1000);
+    });
+}
+
+function remove(){
+
+    let tempoInicial = performance.now();;
+    let db = new PouchDB('db');
+
+    db.get('dados').then(function(doc) {
+        return db.remove(doc);
+    }).then(function (result) {      
+        let tempoFinal = performance.now();;
+        console.log((tempoFinal - tempoInicial)/1000);
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+// -----------------------------------------------------------
+
+function testeInsert(){
+
+    let db = new PouchDB('db');
+
+    db.get('dados').then(function(doc) {
+        return db.remove(doc);
+    }).then(function (result) {      
+        insert();
+    }).catch(function (err) {
+        console.log(err);
+    });
+
+}
+
+function testeRetrieve(){
+    retrieve();
+}
+
+
+function testeRemove(){
+
+    let db = new PouchDB('db');
+
+    // remove();
+
+    db.put({
+        _id: 'dados',
+        nome: dadosTeste         
+    }).then(function (response) {
+        remove();
+    }).catch(function (err) {
+        console.log(err);
+    }); 
+}
